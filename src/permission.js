e3cccadd1e95816cc9bf3fb0980e17acd848dd2b
XiaoLiPing <1151270814@qq.com>
2022-02-28 20:45:43 +0800

    创建

---
+++ src/permission.js
@@ -0,0 +1,74 @@
+import router from './router'
+import store from './store'
+import { Message } from 'element-ui'
+import NProgress from 'nprogress' // 进度条
+import 'nprogress/nprogress.css' // 进度条样式
+import { getToken } from '@/utils/auth' // 从cookie获取令牌
+import getPageTitle from '@/utils/get-page-title'
+
+NProgress.configure({ showSpinner: false }) // NProgress配置
+
+const whiteList = ['/login', '/auth-redirect'] // 没有重定向白名单
+// 全局前置路由守卫，在跳转路由前做判断
+router.beforeEach(async(to, from, next) => {
+  // 开始进度条
+  NProgress.start()
+
+  // set page title
+  document.title = getPageTitle(to.meta.title)
+
+  // determine whether the user has logged in
+  const hasToken = getToken()
+
+  if (hasToken) {
+    if (to.path === '/login') {
+      // 如果已登录，则重定向到主页
+      next({ path: '/' })
+      NProgress.done() // hack: https://github.com/PanJiaChen/vue-element-admin/pull/2939
+    } else {
+      // 通过getInfo判断用户是否获得了权限角色
+      const hasRoles = store.getters.roles && store.getters.roles.length > 0
+      if (hasRoles) {
+        next()
+      } else {
+        try {
+          // 获取用户信息
+          // 注意:roles必须是一个对象数组!例如:['admin']或，['developer'，'editor']
+          const { roles } = await store.dispatch('user/getInfo')
+
+          // 根据角色生成可访问路由映射
+          const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
+
+          // 动态添加可访问路由
+          router.addRoutes(accessRoutes)
+
+          // hack方法确保addRoutes是完整的
+          // 设置replace: true，这样导航就不会留下历史记录
+          next({ ...to, replace: true })
+        } catch (error) {
+          // 移除令牌，进入登录页面重新登录
+          await store.dispatch('user/resetToken')
+          Message.error(error || 'Has Error')
+          next(`/login?redirect=${to.path}`)
+          NProgress.done()
+        }
+      }
+    }
+  } else {
+    /* has no token*/
+
+    if (whiteList.indexOf(to.path) !== -1) {
+      // 在免费登录白名单，直接去
+      next()
+    } else {
+      // 其他没有访问权限的页面被重定向到登录页面。
+      next(`/login?redirect=${to.path}`)
+      NProgress.done()
+    }
+  }
+})
+
+router.afterEach(() => {
+  // finish progress bar
+  NProgress.done()
+})
